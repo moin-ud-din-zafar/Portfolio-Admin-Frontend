cc5c4e9d24c16650937f041811c291857e96c02f
moin ud din zafar <moinseo037@gmail.com>
2025-07-31 17:23:24 +0500

    apii

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const baseURL =
   process.env.REACT_APP_API_URL ||
   import.meta.env.VITE_API_URL ||
-  'https://portfolio-admin-backend-alpha-6rwz3wcuy.vercel.app/api';
+  'https://portfolio-admin-backend-pk.vercel.app/api';
 
 const API = axios.create({ baseURL });
 
